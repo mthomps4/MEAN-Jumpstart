35dd383133f0ef7c0b07136a261886200165ea92
Matthew <matthew.thompson.a@gmail.com>
2016-06-25 16:18:45 -0400

    Express routes api and mongoose added.

--- app/scripts/chirpApp.js
+++ app/scripts/chirpApp.js
@@ -1,4 +1,23 @@
-var app = angular.module('chirpApp', []);
+var app = angular.module('chirpApp', ['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider
+    //the timeline display
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'mainController'
+    })
+    //the login display
+    .when('/login', {
+      templateUrl: 'login.html',
+      controller: 'authController'
+    })
+    //the signup display
+    .when('/register', {
+      templateUrl: 'register.html',
+      controller: 'authController'
+    });
+});
 
 app.controller('mainController', function($scope){
   //models and data binding
@@ -14,5 +33,19 @@ app.controller('mainController', function($scope){
     $scope.posts.push($scope.newPost);
     $scope.newPost = {created_by: '', text:'', created_at:''};//resets posts form
   };
+});
 
+app.controller('authController', function($scope){
+  $scope.user = {username: '', password: ''};
+  $scope.error_message = '';
+
+  $scope.login = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'login request for ' + $scope.user.username;
+  };
+
+  $scope.register = function(){
+    //placeholder until authentication is implemented
+    $scope.error_message = 'registeration request for ' + $scope.user.username;
+  };
 });
